edb2002afa2e98636463065556b3fe4df6c534f8
Tyler Rudman <tyler.rudman@gmail.com>
2022-11-23 18:04:18 -0500

    added hour blocks and display date/time on screen

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+$("#currentDay").text(dayjs().format("MMMM, DD, YYYY HH:mm"));
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
